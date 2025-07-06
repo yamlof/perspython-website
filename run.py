4c87e0077e72e9e0624f8d360be1709da6efb599
Edwin Igbinoba <jamesedwin204@gmail.com>
2025-07-06 20:22:22 +0100

    railway

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import os
 
 app = Flask(__name__)
 
@@ -11,4 +12,5 @@ def projects():
     return render_template("projects.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
